f3d51acfd349458214a633f794c311364af09662
Sean Enderby <sean.enderby@gmail.com>
2016-04-09 16:25:04 +0100

    gsl real transforms;

--- src/atfft_gsl.c
+++ src/atfft_gsl.c
@@ -7,20 +7,24 @@
  */
 
 #include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_eigen.h>
 #include <gsl/gsl_fft_complex.h>
-#include <stdlib.h>
-#include <string.h>
+#include <gsl/gsl_fft_real.h>
+#include <gsl/gsl_fft_halfcomplex.h>
 #include "../include/atfft.h"
 
 struct atfft
 {
     int size;
     int direction;
-    gsl_vector_complex *data;
-    gsl_fft_complex_workspace *workArea;
-    gsl_fft_complex_wavetable *tables;
+    enum atfft_format format;
+    gsl_vector *data;
+    void *workArea;
+    void *tables;
 };
 
 struct atfft* atfft_create (int size, int direction, enum atfft_format format)
@@ -30,25 +34,112 @@ struct atfft* atfft_create (int size, int direction, enum atfft_format format)
     fft = malloc (sizeof (*fft));
     fft->size = size;
     fft->direction = direction;
-    fft->data = gsl_vector_complex_alloc (size);
-    fft->workArea = gsl_fft_complex_workspace_alloc (size);
-    fft->tables = gsl_fft_complex_wavetable_alloc (size);
+    fft->format = format;
+
+    switch (format)
+    {
+        case ATFFT_COMPLEX:
+            fft->data = gsl_vector_alloc (2 * size);
+            fft->workArea = gsl_fft_complex_workspace_alloc (size);
+            fft->tables = gsl_fft_complex_wavetable_alloc (size);
+            break;
+
+        case ATFFT_REAL:
+            fft->data = gsl_vector_alloc (size);
+            fft->workArea = gsl_fft_real_workspace_alloc (size);
+
+            if (format == ATFFT_FORWARD)
+            {
+                fft->tables = gsl_fft_real_wavetable_alloc (size);
+            }
+            else
+            {
+                fft->tables = gsl_fft_halfcomplex_wavetable_alloc (size);
+            }
+
+            break;
+    }
 
     return fft;
 }
 
 void atfft_free (struct atfft *fft)
 {
-    gsl_fft_complex_wavetable_free (fft->tables);
-    gsl_fft_complex_workspace_free (fft->workArea);
-    gsl_vector_complex_free (fft->data);
+    switch (fft->format)
+    {
+        case ATFFT_COMPLEX:
+            gsl_fft_complex_wavetable_free (fft->tables);
+            gsl_fft_complex_workspace_free (fft->workArea);
+            break;
+
+        case ATFFT_REAL:
+            if (fft->format == ATFFT_FORWARD)
+            {
+                gsl_fft_real_wavetable_free (fft->tables);
+            }
+            else
+            {
+                gsl_fft_halfcomplex_wavetable_free (fft->tables);
+            }
+
+            gsl_fft_real_workspace_free (fft->workArea);
+            break;
+    }
+
+    gsl_vector_free (fft->data);
+
     free (fft);
 }
 
 void atfft_complex_transform (struct atfft *fft, double *in, double *out)
 {
     size_t nBytes = 2 * fft->size * sizeof (*in);
-    memcpy (fft->data->data, in, nBytes);
-    gsl_fft_complex_transform (fft->data->data, fft->data->stride, fft->data->size, fft->tables, fft->workArea, fft->direction);
-    memcpy (out, fft->data->data, nBytes);
+    gsl_complex_packed_array data = fft->data->data;
+
+    /* Only to be used with complex FFTs. */
+    assert (fft->format == ATFFT_COMPLEX);
+
+    memcpy (data, in, nBytes);
+    gsl_fft_complex_transform (data, fft->data->stride, fft->size, fft->tables, fft->workArea, fft->direction);
+    memcpy (out, data, nBytes);
+}
+
+void atfft_half_complex_gsl_to_fftw (double *in, double *out, int size)
+{
+    out [0] = in [0];
+    out [1] = 0;
+
+    memcpy (out + 2, in + 1, (size - 1) * sizeof (*out));
+
+    out [size + 1] = 0;
+}
+
+void atfft_real_forward_transform (struct atfft *fft, double *in, double *out)
+{
+    double *data = fft->data->data;
+
+    /* Only to be used for forward real FFTs. */
+    assert ((fft->format == ATFFT_REAL) && (fft->direction == ATFFT_FORWARD));
+
+    memcpy (data, in, fft->size * sizeof (*data));
+    gsl_fft_real_transform (data, fft->data->stride, fft->size, fft->tables, fft->workArea);
+    atfft_half_complex_gsl_to_fftw (data, out, fft->size);
+}
+
+void atfft_half_complex_fftw_to_gsl (double *in, double *out, int size)
+{
+    out [0] = in [0];
+    memcpy (out + 1, in + 2, (size - 1) * sizeof (*out));
+}
+
+void atfft_real_backward_transform (struct atfft *fft, double *in, double *out)
+{
+    double *data = fft->data->data;
+
+    /* Only to be used for backward real FFTs. */
+    assert ((fft->format == ATFFT_REAL) && (fft->direction == ATFFT_BACKWARD));
+
+    atfft_half_complex_fftw_to_gsl (in, data, fft->size);
+    gsl_fft_halfcomplex_transform (data, fft->data->stride, fft->size, fft->tables, fft->workArea);   
+    memcpy (out, data, fft->size * sizeof (*data));
 }
