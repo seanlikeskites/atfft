6830bb79d8de154c19ede325f22c2a761a60498e
Sean Enderby <sean.enderby@gmail.com>
2016-04-13 08:20:33 +0100

    removed some includes we dont need

--- src/atfft_ffts.c
+++ src/atfft_ffts.c
@@ -9,7 +9,6 @@
  */
 
 #include <stdlib.h>
-#include <string.h>
 #include <assert.h>
 #include <ffts/ffts.h>
 #include <atfft.h>
--- src/atfft_vdsp.c
+++ src/atfft_vdsp.c
@@ -9,7 +9,6 @@
  */
 
 #include <stdlib.h>
-#include <string.h>
 #include <assert.h>
 #include <Accelerate/Accelerate.h>
 #include <atfft.h>
