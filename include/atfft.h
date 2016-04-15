5b48030cea24accf68177720f821a0d5a072dc4d
Sean Enderby <sean.enderby@gmail.com>
2016-04-15 14:54:07 +0100

    doxygen comes to bring understanding

--- include/atfft.h
+++ include/atfft.h
@@ -14,19 +14,35 @@
 #define ATFFT_FORWARD -1
 #define ATFFT_BACKWARD 1
 
+/**
+ *
+ *
+ */
 enum atfft_format
 {
     ATFFT_COMPLEX,
     ATFFT_REAL
 };
 
+/** 
+ * A Structure to hold internal FFT implementation.
+ * 
+ * When using atfft you will create one of these structures
+ * using atfft_create(), this structure is then passed 
+ * to the calculation functions in order to compute DFTs.
+ */
 struct atfft;
 
+/** A complex float type. */
 typedef float atfft_complex_f [2];
+/** A complex double type. */
 typedef double atfft_complex_d [2];
+/** A complex long double type. */
 typedef long double atfft_complex_l [2];
 
+/** A macro to return the real part of the complex types. */
 #define ATFFT_REAL(x) ((x) [0])
+/** A macro to return the imaginary part of the complex types. */
 #define ATFFT_IMAG(x) ((x) [1])
 
 #if defined(ATFFT_TYPE_FLOAT)
@@ -43,19 +59,81 @@ typedef long double atfft_complex_l [2];
     typedef atfft_complex_d atfft_complex;
 #endif
 
-/* some handy integer tests */
+/**
+ * Check if an integer is even.
+ *
+ * @param x integer to check
+ */
 int atfft_is_even (unsigned int x);
+
+/**
+ * Check if an integer is odd.
+ *
+ * @param x integer to check
+ */
 int atfft_is_odd (unsigned int x);
+
+/**
+ * Check if an integer is a power of 2
+ *
+ * @param x integer to check
+ */
 int atfft_is_power_of_2 (unsigned int x);
 
-/* normalisation */
+/**
+ * Normalise a real DFT output.
+ *
+ * Applies 1 / size scaling to a real valued signal.
+ *
+ * @param data the signal to normalise (should contain size elements)
+ * @param size the length of the signal
+ */
 void atfft_normalise_real (atfft_sample *data, int size);
+
+/**
+ * Normalise a complex DFT output.
+ * 
+ * Applies 1 / size scaling to a complex valued signal.
+ *
+ * @param data the signal to normalise (should contain size elements)
+ * @param size the length of the signal
+ */
 void atfft_normalise_complex (atfft_complex *data, int size);
 
-/* conversion functions */
+/**
+ * Get the real part of a complex signal.
+ *
+ * @param in the input signal (should contain size elements)
+ * @param out the output signal (should contain size elements)
+ * @param size the length of the signals
+ */
 void atfft_real (atfft_complex *in, atfft_sample *out, int size);
+
+/**
+ * Get the imaginary part of a complex signal.
+ *
+ * @param in the input signal (should contain size elements)
+ * @param out the output signal (should contain size elements)
+ * @param size the length of the signals
+ */
 void atfft_imag (atfft_complex *in, atfft_sample *out, int size);
+
+/**
+ * Create a complex signal from a real signal.
+ *
+ * @param in the input signal (should contain size elements)
+ * @param out the output signal (should contain size elements)
+ * @param size the length of the signals
+ */
 void atfft_real_to_complex (atfft_sample *in, atfft_complex *out, int size);
+
+/**
+ * Create a complex signal from a halfcomplex signal.
+ *
+ * @param in the input signal (should contain size / 2 + 1 elements)
+ * @param out the output signal (should contain size elements)
+ * @param size the length of the output signal
+ */
 void atfft_halfcomplex_to_complex (atfft_complex *in, atfft_complex *out, int size);
 
 #ifndef ATFFT_TYPE_FLOAT
@@ -79,9 +157,22 @@ void atfft_long_double_to_sample_complex (atfft_complex_l *in, atfft_complex *ou
 void atfft_sample_to_long_double_complex (atfft_complex *in, atfft_complex_l *out, int size);
 #endif
 
-/* fft calculation */
+/**
+ * Create an fft structure.
+ *
+ * @param size the signal length the fft should operate on
+ * @param direction the direction of the transform
+ * @format the type of transform (real or complex)
+ */
 struct atfft* atfft_create (int size, int direction, enum atfft_format format);
+
+/**
+ * Free an fft structure.
+ *
+ * @param fft the structure to free
+ */
 void atfft_free (struct atfft *fft);
+
 void atfft_complex_transform (struct atfft *fft, atfft_complex *in, atfft_complex *out);
 void atfft_real_forward_transform (struct atfft *fft, atfft_sample *in, atfft_complex *out);
 void atfft_real_backward_transform (struct atfft *fft, atfft_complex *in, atfft_sample *out);
