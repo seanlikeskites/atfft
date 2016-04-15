e2513af525b9aca4e0225188417357a6f7bb4e70
seanlikeskites <sean.enderby@gmail.com>
2016-04-15 13:53:22 +0100

    vdsp types

--- src/atfft_vdsp.c
+++ src/atfft_vdsp.c
@@ -13,13 +13,30 @@
 #include <Accelerate/Accelerate.h>
 #include <atfft.h>
 
+#ifdef ATFFT_TYPE_FLOAT
+#   define ATFFT_VDSP_DFT_SETUP vDSP_DFT_Setup
+#   define ATFFT_VDSP_DFT_ZOP_CREATE_SETUP vDSP_DFT_zop_CreateSetup
+#   define ATFFT_VDSP_DFT_ZROP_CREATE_SETUP vDSP_DFT_zrop_CreateSetup
+#   define ATFFT_VDSP_DFT_DESTROY_SETUP vDSP_DFT_DestroySetup
+#   define ATFFT_VDSP_DFT_EXECUTE vDSP_DFT_Execute
+    typedef float atfft_vdsp_sample;
+
+#else
+#   define ATFFT_VDSP_DFT_SETUP vDSP_DFT_SetupD
+#   define ATFFT_VDSP_DFT_ZOP_CREATE_SETUP vDSP_DFT_zop_CreateSetupD
+#   define ATFFT_VDSP_DFT_ZROP_CREATE_SETUP vDSP_DFT_zrop_CreateSetupD
+#   define ATFFT_VDSP_DFT_DESTROY_SETUP vDSP_DFT_DestroySetupD
+#   define ATFFT_VDSP_DFT_EXECUTE vDSP_DFT_ExecuteD
+    typedef double atfft_vdsp_sample;
+#endif
+
 struct atfft
 {
     int size;
     int direction;
     enum atfft_format format;
-    double *inR, *inI, *outR, *outI;
-    vDSP_DFT_SetupD setup;
+    atfft_vdsp_sample *inR, *inI, *outR, *outI;
+    ATFFT_VDSP_DFT_SETUP setup;
 };
 
 int atfft_is_supported_length_vdsp (unsigned int length, enum atfft_format format)
@@ -67,11 +84,11 @@ struct atfft* atfft_create (int size, int direction, enum atfft_format format)
     switch (format)
     {
         case ATFFT_COMPLEX:
-            fft->setup = vDSP_DFT_zop_CreateSetupD (NULL, size, -direction);
+            fft->setup = ATFFT_VDSP_DFT_ZOP_CREATE_SETUP (NULL, size, -direction);
             break;
 
         case ATFFT_REAL:
-            fft->setup = vDSP_DFT_zrop_CreateSetupD (NULL, size, -direction);
+            fft->setup = ATFFT_VDSP_DFT_ZROP_CREATE_SETUP (NULL, size, -direction);
             break;
     }
 
@@ -80,7 +97,7 @@ struct atfft* atfft_create (int size, int direction, enum atfft_format format)
 
 void atfft_free (struct atfft *fft)
 {
-    vDSP_DFT_DestroySetupD (fft->setup);
+    ATFFT_VDSP_DFT_DESTROY_SETUP (fft->setup);
     free (fft->outI);
     free (fft->outR);
     free (fft->inI);
@@ -88,7 +105,18 @@ void atfft_free (struct atfft *fft)
     free (fft);
 }
 
-void atfft_complex_vdsp_to_fftw (double *real, double *imag, atfft_complex_double *out, int size)
+void atfft_complex_fftw_to_vdsp (atfft_complex *in, atfft_vdsp_sample *real, atfft_vdsp_sample *imag, int size)
+{
+    int i = 0;
+
+    for (i = 0; i < size; ++i)
+    {
+        real [i] = ATFFT_REAL (in [i]);
+        imag [i] = ATFFT_IMAG (in [i]);
+    }
+}
+
+void atfft_complex_vdsp_to_fftw (atfft_vdsp_sample *real, atfft_vdsp_sample *imag, atfft_complex *out, int size)
 {
     int i = 0;
 
@@ -99,18 +127,17 @@ void atfft_complex_vdsp_to_fftw (double *real, double *imag, atfft_complex_doubl
     }
 }
 
-void atfft_complex_transform (struct atfft *fft, atfft_complex_double *in, atfft_complex_double *out)
+void atfft_complex_transform (struct atfft *fft, atfft_complex *in, atfft_complex *out)
 {
     /* Only to be used with complex FFTs. */
     assert (fft->format == ATFFT_COMPLEX);
 
-    atfft_real (in, fft->inR, fft->size);
-    atfft_imag (in, fft->inI, fft->size);
-    vDSP_DFT_ExecuteD (fft->setup, fft->inR, fft->inI, fft->outR, fft->outI);
+    atfft_complex_fftw_to_vdsp (in, fft->inR, fft->inI, fft->size);
+    ATFFT_VDSP_DFT_EXECUTE (fft->setup, fft->inR, fft->inI, fft->outR, fft->outI);
     atfft_complex_vdsp_to_fftw (fft->outR, fft->outI, out, fft->size);
 }
 
-void atfft_real_fftw_to_vdsp (double *in, double *outE, double *outO, int size)
+void atfft_real_fftw_to_vdsp (atfft_sample *in, atfft_vdsp_sample *outE, atfft_vdsp_sample *outO, int size)
 {
     int i = 0;
 
@@ -121,7 +148,7 @@ void atfft_real_fftw_to_vdsp (double *in, double *outE, double *outO, int size)
     }
 }
 
-void atfft_halfcomplex_vdsp_to_fftw (double *inE, double *inO, atfft_complex_double *out, int size)
+void atfft_halfcomplex_vdsp_to_fftw (atfft_vdsp_sample *inE, atfft_vdsp_sample *inO, atfft_complex *out, int size)
 {
     int i = 0;
     int halfSize = size / 2;
@@ -139,17 +166,17 @@ void atfft_halfcomplex_vdsp_to_fftw (double *inE, double *inO, atfft_complex_dou
     ATFFT_IMAG (out [halfSize]) = 0;
 }
 
-void atfft_real_forward_transform (struct atfft *fft, double *in, atfft_complex_double *out)
+void atfft_real_forward_transform (struct atfft *fft, atfft_sample *in, atfft_complex *out)
 {
     /* Only to be used for forward real FFTs. */
     assert ((fft->format == ATFFT_REAL) && (fft->direction == ATFFT_FORWARD));
 
     atfft_real_fftw_to_vdsp (in, fft->inR, fft->inI, fft->size);
-    vDSP_DFT_ExecuteD (fft->setup, fft->inR, fft->inI, fft->outR, fft->outI);
+    ATFFT_VDSP_DFT_EXECUTE (fft->setup, fft->inR, fft->inI, fft->outR, fft->outI);
     atfft_halfcomplex_vdsp_to_fftw (fft->outR, fft->outI, out, fft->size);
 }
 
-void atfft_halfcomplex_fftw_to_vdsp (atfft_complex_double *in, double *outE, double *outO, int size)
+void atfft_halfcomplex_fftw_to_vdsp (atfft_complex *in, atfft_vdsp_sample *outE, atfft_vdsp_sample *outO, int size)
 {
     int i = 0;
     int halfSize = size / 2;
@@ -165,7 +192,7 @@ void atfft_halfcomplex_fftw_to_vdsp (atfft_complex_double *in, double *outE, dou
     outO [0] = ATFFT_REAL (in [halfSize]);
 }
 
-void atfft_real_vdsp_to_fftw (double *inE, double *inO, double *out, int size)
+void atfft_real_vdsp_to_fftw (atfft_vdsp_sample *inE, atfft_vdsp_sample *inO, atfft_sample *out, int size)
 {
     int i = 0;
 
@@ -176,13 +203,13 @@ void atfft_real_vdsp_to_fftw (double *inE, double *inO, double *out, int size)
     }
 }
 
-void atfft_real_backward_transform (struct atfft *fft, atfft_complex_double *in, double *out)
+void atfft_real_backward_transform (struct atfft *fft, atfft_complex *in, atfft_sample *out)
 {
     /* Only to be used for backward real FFTs. */
     assert ((fft->format == ATFFT_REAL) && (fft->direction == ATFFT_BACKWARD));
 
     atfft_halfcomplex_fftw_to_vdsp (in, fft->inR, fft->inI, fft->size);
-    vDSP_DFT_ExecuteD (fft->setup, fft->inR, fft->inI, fft->outR, fft->outI);
+    ATFFT_VDSP_DFT_EXECUTE (fft->setup, fft->inR, fft->inI, fft->outR, fft->outI);
     atfft_real_vdsp_to_fftw (fft->outR, fft->outI, out, fft->size);
 }
 
